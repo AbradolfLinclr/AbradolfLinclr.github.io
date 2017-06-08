eb019589c78825801f9d7c9257b0e961c563b0af
Cody Maheu <Abradolf_linclr@outlook.com>
2017-06-07 23:35:01 -0700

    hopefully project page complete

--- about-page/about-js.js
+++ about-page/about-js.js
@@ -7,17 +7,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
 }
-
-
-$(window).scroll(function () { 
-if ($(window).scrollTop() > $('body').height() / 6) {
-$('#upArrow').fadeTo(25, 1);
-} 
-if ($(window).scrollTop() < $('body').height() / 6) {
-$('#upArrow').fadeTo(25, 0);
-} 
-});
-
-$('#upArrow').click(function(){
-$("html, body").animate({ scrollTop: 0 }, "slow");
-});
